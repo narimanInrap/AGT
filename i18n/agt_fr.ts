1e135651e097e18f9d40576e4e44e530bb989b05
Nariman <nariman.hatami@inrap.fr>
2017-05-29 12:37:21 +0200

    Add files via upload

--- i18n/agt_fr.ts
+++ i18n/agt_fr.ts
@@ -2,39 +2,137 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="fr_FR" sourcelanguage="en">
 <context>
-    <name>ArcheoPhi</name>
+    <name>AGT</name>
     <message>
-        <location filename="archeophi.py" line="212"/>
-        <source>&amp;ArcheoPhi</source>
-        <translation>&amp;ArcheoPhi</translation>
+        <location filename="agt.py" line="227"/>
+        <source>&amp;AGT</source>
+        <translation>&amp;AGT</translation>
     </message>
     <message>
-        <location filename="archeophi.py" line="176"/>
+        <location filename="agt.py" line="186"/>
         <source>RM15/RM85 processing</source>
-        <translation>traitements RM15/RM85</translation>
+        <translation>Traitements RM15/RM85</translation>
     </message>
     <message>
-        <location filename="archeophi.py" line="183"/>
+        <location filename="agt.py" line="193"/>
         <source>MXPDA processing</source>
-        <translation>traitements MXPDA</translation>
+        <translation>Traitements MXPDA</translation>
     </message>
     <message>
-        <location filename="archeophi.py" line="197"/>
+        <location filename="agt.py" line="218"/>
         <source>help</source>
         <translation>aide</translation>
     </message>
+    <message>
+        <location filename="agt.py" line="179"/>
+        <source>RM15/RM85 download</source>
+        <translation>Chargement RM15/RM85</translation>
+    </message>
 </context>
 <context>
-    <name>ArcheoPhiElectDialogBase</name>
+    <name>AGTElecDownDialog</name>
+    <message>
+        <location filename="ui_ElecDownDialog.py" line="181"/>
+        <source>RM15/RM85 download</source>
+        <translation>Chargement RM15/RM85</translation>
+    </message>
+    <message>
+        <location filename="ui_ElecDownDialog.py" line="182"/>
+        <source>Output file</source>
+        <translation>Fichier de sortie</translation>
+    </message>
+    <message>
+        <location filename="ui_ElecDownDialog.py" line="183"/>
+        <source>RM15</source>
+        <translation>RM15</translation>
+    </message>
+    <message>
+        <location filename="ui_ElecDownDialog.py" line="184"/>
+        <source>RM85</source>
+        <translation>RM85</translation>
+    </message>
+    <message>
+        <location filename="ui_ElecDownDialog.py" line="185"/>
+        <source>Number of grids</source>
+        <translation>Nombre de grilles</translation>
+    </message>
+    <message>
+        <location filename="ui_ElecDownDialog.py" line="186"/>
+        <source>Grid size (m)</source>
+        <translation>Taille des grilles (m)</translation>
+    </message>
+    <message>
+        <location filename="ui_ElecDownDialog.py" line="187"/>
+        <source>browse</source>
+        <translation>parcourir</translation>
+    </message>
+    <message>
+        <location filename="ui_ElecDownDialog.py" line="188"/>
+        <source>Number of probes</source>
+        <translation>Nombre d&apos;électrodes</translation>
+    </message>
+    <message>
+        <location filename="ui_ElecDownDialog.py" line="189"/>
+        <source>Probe configuration</source>
+        <translation>Configuration des électrodes</translation>
+    </message>
+    <message>
+        <location filename="ui_ElecDownDialog.py" line="190"/>
+        <source>Line step (m)</source>
+        <translation>Pas en Y (m)</translation>
+    </message>
+    <message>
+        <location filename="ui_ElecDownDialog.py" line="191"/>
+        <source>Probe spacing (m)</source>
+        <translation>Distance inter-électrode (m)</translation>
+    </message>
+    <message>
+        <location filename="ui_ElecDownDialog.py" line="192"/>
+        <source>Number of channels</source>
+        <translation>Nombre de voies</translation>
+    </message>
+    <message>
+        <location filename="ui_ElecDownDialog.py" line="193"/>
+        <source> X</source>
+        <translation>X</translation>
+    </message>
+    <message>
+        <location filename="ui_ElecDownDialog.py" line="194"/>
+        <source>Current intensity (mA)</source>
+        <translation>Intensité du courrant (mA)</translation>
+    </message>
+    <message>
+        <location filename="ui_ElecDownDialog.py" line="196"/>
+        <source>COM port</source>
+        <translation>Port COM</translation>
+    </message>
+    <message>
+        <location filename="ui_ElecDownDialog.py" line="197"/>
+        <source>Baud rate</source>
+        <translation>Baud</translation>
+    </message>
+    <message>
+        <location filename="ui_ElecDownDialog.py" line="198"/>
+        <source>run</source>
+        <translation>lancer</translation>
+    </message>
+    <message>
+        <location filename="ui_ElecDownDialog.py" line="195"/>
+        <source>Number of collected points/step</source>
+        <translation>Nombre de points mesurés par pas</translation>
+    </message>
+</context>
+<context>
+    <name>AGTElectDialogBase</name>
     <message>
         <location filename="ui_electDialog.py" line="157"/>
         <source>RM15/RM85 processing</source>
-        <translation>traitements RM15/RM85</translation>
+        <translation>Traitements RM15/RM85</translation>
     </message>
     <message>
         <location filename="ui_electDialog.py" line="158"/>
         <source>Raw data (.dat)</source>
-        <translation>données brutes</translation>
+        <translation>Données brutes</translation>
     </message>
     <message>
         <location filename="ui_electDialog.py" line="159"/>
@@ -49,7 +147,7 @@
     <message>
         <location filename="ui_electDialog.py" line="161"/>
         <source>Median value percentage %   </source>
-        <translation>pourcentage pour le filtre médiane %</translation>
+        <translation>Pourcentage pour le filtre médiane %</translation>
     </message>
     <message>
         <location filename="ui_electDialog.py" line="162"/>
@@ -59,7 +157,7 @@
     <message>
         <location filename="ui_electDialog.py" line="163"/>
         <source>Kernel size      </source>
-        <translation>taille du masque</translation>
+        <translation>Taille du masque</translation>
     </message>
     <message>
         <location filename="ui_electDialog.py" line="164"/>
@@ -88,7 +186,7 @@
     </message>
 </context>
 <context>
-    <name>ArcheoPhiGeorefDialog</name>
+    <name>AGTGeorefDialog</name>
     <message>
         <location filename="ui_georefDialog.py" line="169"/>
         <source>Georeferencing</source>
@@ -131,16 +229,44 @@
     </message>
 </context>
 <context>
-    <name>ArcheoPhiMagDialog</name>
+    <name>AGTGridDialog</name>
+    <message>
+        <location filename="ui_GridDialog.py" line="95"/>
+        <source>Grid&apos;s first point X coordinate</source>
+        <translation>Longitude (X) du premierpoint de la grille</translation>
+    </message>
+    <message>
+        <location filename="ui_GridDialog.py" line="96"/>
+        <source>Grid&apos;s name (number)</source>
+        <translation>Nom(numéro) de la grille</translation>
+    </message>
+    <message>
+        <location filename="ui_GridDialog.py" line="97"/>
+        <source>Grid&apos;s first point Y coordinate</source>
+        <translation>Latitude (Y) du premierpoint de la grille</translation>
+    </message>
+    <message>
+        <location filename="ui_GridDialog.py" line="94"/>
+        <source>Grid Dialog</source>
+        <translation>Grid Dialog</translation>
+    </message>
+    <message>
+        <location filename="ui_GridDialog.py" line="98"/>
+        <source>Apply</source>
+        <translation>Appliquer</translation>
+    </message>
+</context>
+<context>
+    <name>AGTMagDialog</name>
     <message>
         <location filename="ui_magDialog.py" line="277"/>
         <source>MXPDA processing</source>
-        <translation>traitements MXPDA</translation>
+        <translation>Traitements MXPDA</translation>
     </message>
     <message>
         <location filename="ui_magDialog.py" line="278"/>
         <source>Raw data (.asc)</source>
-        <translation>données brutes</translation>
+        <translation>Données brutes</translation>
     </message>
     <message>
         <location filename="ui_magDialog.py" line="284"/>
@@ -155,7 +281,7 @@
     <message>
         <location filename="ui_magDialog.py" line="281"/>
         <source>CRS projection</source>
-        <translation>prjection SCR</translation>
+        <translation>Projection SCR</translation>
     </message>
     <message>
         <location filename="ui_magDialog.py" line="282"/>
@@ -165,7 +291,7 @@
     <message>
         <location filename="ui_magDialog.py" line="283"/>
         <source>Add coordinates fields</source>
-        <translation>L&apos;ajout des champs de coordonnées</translation>
+        <translation>Ajout des champs de coordonnées</translation>
     </message>
     <message>
         <location filename="ui_magDialog.py" line="285"/>
@@ -224,7 +350,7 @@
     </message>
 </context>
 <context>
-    <name>ArcheoPhiRasterDialog</name>
+    <name>AGTRasterDialog</name>
     <message>
         <location filename="ui_rasterDialog.py" line="138"/>
         <source>Interpolation</source>
@@ -269,6 +395,19 @@
         <location filename="ui_rasterDialog.py" line="147"/>
         <source>Interpolation process</source>
         <translation>Traitement par interpolation</translation>
+    </message>
+</context>
+<context>
+    <name>ElecDownDlg</name>
+    <message>
+        <location filename="ElecDownDialog.py" line="97"/>
+        <source>Please specify an output file name.</source>
+        <translation>Veuillez renseigner un fichier de sortie.</translation>
+    </message>
+    <message>
+        <location filename="ElecDownDialog.py" line="121"/>
+        <source>Unexpected error, procedure stopped.</source>
+        <translation>Erreur inattendue, la procédure s&apos;est arrêtée.</translation>
     </message>
 </context>
 <context>
@@ -328,6 +467,31 @@
         <source>The project name you have chosen already exists, you might overwrite some shapefiles. Do you want to continue?</source>
         <translation>Le nom de projet choisi existe déjà, vous risquez d&apos;écraser des shapefiles. Voulez-vous cotinuer?</translation>
     </message>
+    <message>
+        <location filename="ElecDownDialog.py" line="115"/>
+        <source>Procedure canceled.</source>
+        <translation>Procédure annulée.</translation>
+    </message>
+</context>
+<context>
+    <name>Exceptions</name>
+    <message>
+        <location filename="AGTExceptions.py" line="39"/>
+        <source>Error deleting Shapefile {}.</source>
+        <translation>Erreur de suppresion du Shapefile {}.</translation>
+    </message>
+    <message>
+        <location filename="AGTExceptions.py" line="50"/>
+        <source>No feature was created. The shapefile was deleted {}.
+</source>
+        <translation>Aucune forme n&apos;a été créé. Le shpefile a été supprimé{}.</translation>
+    </message>
+    <message>
+        <location filename="AGTExceptions.py" line="61"/>
+        <source>Error reading {}.
+</source>
+        <translation>Erreur de suppression {}.</translation>
+    </message>
 </context>
 <context>
     <name>MagDlg</name>
@@ -358,6 +522,19 @@
     </message>
 </context>
 <context>
+    <name>Utility</name>
+    <message>
+        <location filename="AGTUtilities.py" line="98"/>
+        <source>pole-pole</source>
+        <translation>pôle-pôle</translation>
+    </message>
+    <message>
+        <location filename="AGTUtilities.py" line="141"/>
+        <source>Save output file</source>
+        <translation>Sauvegarder le fichier de sortie</translation>
+    </message>
+</context>
+<context>
     <name>georefDlg</name>
     <message>
         <location filename="georefDialog.py" line="81"/>
@@ -368,6 +545,19 @@
         <location filename="georefDialog.py" line="94"/>
         <source>All point coordinates must be floating point numbers.</source>
         <translation>Les coordonnées des points doivent être des nombres réels (float).</translation>
+    </message>
+</context>
+<context>
+    <name>gridDlg</name>
+    <message>
+        <location filename="GridDialog.py" line="52"/>
+        <source>Please fill out all fields.</source>
+        <translation>Veuilez renseigner tous les champs.</translation>
+    </message>
+    <message>
+        <location filename="GridDialog.py" line="46"/>
+        <source>grid number </source>
+        <translation>grille numéro </translation>
     </message>
 </context>
 </TS>
